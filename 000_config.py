6b81dd39ca5617305eb91ccdf437492986c7e442
midas628 <midas628@163.com>
2019-02-12 23:15:18 +0800

    Create 000_config.py

---
+++ 000_config.py
@@ -0,0 +1,22 @@
+
+#!-*-coding:utf-8 -*-
+#@TIME    : 2019/2/11/00 20 11:20
+#@Author  : GDM
+
+
+x_mt_appid='x-mt-appid:mtoilet'
+x_mt_channel='x-mt-channel:ringle'
+x_mt_devid='x-mt-devid:'
+x_mt_devtype='x-mt-devtype:'
+x_mt_platform='x-mt-platform:android'
+x_mt_rid_none='x-mt-rid:NONE'
+#x_mt_token_str='x-mt-token:'+x_mt_token
+x_mt_token_str_none='x-mt-token:NONE'
+x_mt_version='x-mt-version:2.0.31'
+version='2.0.31'
+sequence='x-mt-appid;x-mt-channel;x-mt-devid;x-mt-devtype;x-mt-platform;x-mt-rid;x-mt-token;x-mt-version'
+
+code1='da39a3ee5e6b4b0d3255bfef95601890afd80709'
+code2='c3cfba79efd8f1c0f77473ec4a3818ac'
+
+
